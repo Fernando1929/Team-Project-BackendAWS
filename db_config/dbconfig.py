12c54e5cb34cf13b5e5b48f4641cfcf613a0e40a
Luis Quiles <luis.quiles1@upr.edu>
2021-08-09 14:51:36 -0500

    added the database env vars.

--- db_config/dbconfig.py
+++ db_config/dbconfig.py
@@ -1,5 +1,6 @@
-pg_config={
-    'user' : 'dbuser',
-    'passwd' : 'dbuser12',
-    'dbname' : 'dbtesting'
+mysql_config={
+    'DB_WEBSERVER':'http://database-1.cwroyeeic3cx.ushttp://database-1.cwroyeeic3cx.us-east-1.rds.amazonaws.comhttp://east-1.rds.amazonaws.com',
+    'DB_USERNAME' : 'admin',
+    'DB_PASSWORD' : 'admin12',
+    'DB_DATABASE' : 'dbtesting'
 }
