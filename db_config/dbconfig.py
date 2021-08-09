562d85d07797438045dabfc053d51e4ef771c54f
Luis Quiles <luis.quiles1@upr.edu>
2021-08-09 15:34:13 -0500

    Fixed db credentials

--- db_config/dbconfig.py
+++ db_config/dbconfig.py
@@ -1,5 +1,5 @@
 mysql_config={
-    'DB_WEBSERVER':'http://database-1.cwroyeeic3cx.ushttp://database-1.cwroyeeic3cx.us-east-1.rds.amazonaws.comhttp://east-1.rds.amazonaws.com',
+    'DB_WEBSERVER':'database-1.cwroyeeic3cx.us-east-1.rds.amazonaws.com',
     'DB_USERNAME' : 'admin',
     'DB_PASSWORD' : 'admin12',
     'DB_DATABASE' : 'dbtesting'
