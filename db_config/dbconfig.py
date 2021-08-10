76fada4cafd7c43af7535ae171685bd5fefb06b2
Luis Quiles <luis.quiles1@upr.edu>
2021-08-10 10:38:17 -0500

    Added the schema and fixed configuration

--- db_config/dbconfig.py
+++ db_config/dbconfig.py
@@ -1,6 +1,6 @@
 mysql_config={
     'DB_WEBSERVER':'database-1.cwroyeeic3cx.us-east-1.rds.amazonaws.com',
     'DB_USERNAME' : 'admin',
-    'DB_PASSWORD' : 'admin12',
+    'DB_PASSWORD' : 'Admin1234',
     'DB_DATABASE' : 'Sample'
 }
