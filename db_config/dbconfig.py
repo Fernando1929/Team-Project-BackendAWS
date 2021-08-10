b9ae50cdbf36d94bd60515baa1fa110926266b83
Nomar Medina <nomar.medina@upr.edu>
2021-08-10 10:51:06 -0500

    Put correct info on the dbconfig file and added schema made by Fernando.

--- db_config/dbconfig.py
+++ db_config/dbconfig.py
@@ -1,6 +1,6 @@
 mysql_config={
-    'DB_WEBSERVER':'http://database-1.cwroyeeic3cx.ushttp://database-1.cwroyeeic3cx.us-east-1.rds.amazonaws.comhttp://east-1.rds.amazonaws.com',
+    'DB_WEBSERVER':'http://database-1.cwroyeeic3cx.us-east-1.rds.amazonaws.com',
     'DB_USERNAME' : 'admin',
-    'DB_PASSWORD' : 'admin12',
-    'DB_DATABASE' : 'dbtesting'
+    'DB_PASSWORD' : 'Admin1234',
+    'DB_DATABASE' : 'Sample'
 }
